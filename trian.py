9a2318b92124b6af87c02680c43ee2cb43bccecd
Ismail Elhassouni <ielhasso@e1r7p5.1337.ma>
2026-07-02 22:06:04 +0100

    not enough

--- trian.py
+++ trian.py
@@ -1,28 +1,35 @@
-def estimated(theta0,theta1,mileage):
+import csv
+def estimated(theta0=0,theta1=0,mileage=0):
     return theta0 + (theta1 * mileage)
 
 
 def load():
-    file = open("data.csv","r")
-    prices =[]
-    km = []
-    for line in file:
-        if line !="km,price\n":
-            line =line.strip()
-            line = line.split(",")
-            prices.append(float(line[1]))
-            km.append(float(line[0]))
-    return prices,km
+    with  open("data.csv","r") as f:
+        reader = csv.reader(f)
+        next(reader)
+        prices =[]
+        km = []
+        for line in reader:
+                km.append(float(line[0]))
+                prices.append(float(line[1]))
+        return prices,km
 
-def train(fielname,learing_rate=0.000001,iterations=1000):
+def train(learing_rate=0.0001,iterations=1000):
     prices,km = load()
     theta0 = 0
     theta1 = 0
-    m = len(prices)
+    m = len(prices)#numbers of cars 
     for i in range(iterations):
         sum0 = 0
         sum1 = 0
         predictions = [estimated(theta0,theta1,x) for x in km]
-        erros = [predictions[j] - prices[j] for j in range(m)]
-        sum_erros = sum(erros)
-        sum_errors_km = sum()
+        errors = [predictions[j] - prices[j] for j in range(m)]
+        sum_errors = sum(errors)
+        sum_errors_km = sum([e*m_val for e,m_val in zip(errors,km)])
+        tmpO = theta0 - (learing_rate * (1/m) * sum_errors)
+        tmp1 = theta1 - ( learing_rate * ( 1 / m) * sum_errors_km)
+        theta0 = tmpO
+        theta1 = tmp1
+    with open('thetas','w') as f:
+        f.write(f"{theta0}\n{theta1}")
+train()
